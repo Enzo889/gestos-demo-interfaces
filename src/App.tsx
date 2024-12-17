252d87ddbb8eea63b74dc54db288e81190d6fbfe
Enzo Bustamante <enzobustamante895@gmail.com>
2024-12-17 18:40:10 -0300

    feat: add font, dark mode and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import "./App.css";
+import { ModeToggle } from "./components/mode-toggle";
+import { TabsDemo } from "./components/tabs-content";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Demos</h1>
+      <div className="flex flex-col items-center  m-auto h-screen max-w-md ">
+        <div className="flex justify-between  items-center w-full py-4 px-8 ">
+          <h1>Demos</h1>
+          <ModeToggle />
+        </div>
+        <div>
+          {" "}
+          <TabsDemo />{" "}
+        </div>
       </div>
     </>
   );
---
+++ src/components/tabs-content.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export function TabsDemo() {
+  return (
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <p>first card</p>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <p>second card</p>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
+}
